e7054ebc7dd424dcbb2c6e11674044264afa2ddd
Pokeknight2610 <118559005+Pokeknight2610@users.noreply.github.com>
2024-05-13 13:20:32 -0700

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,3 +1,3 @@
 if (window.location.href.includes("youtube")) {
-    window.location.href = "https://pokeknight2610.github.io/blockedpage/blocked1.mhtml";
+    window.location.href = "https://pokeknight2610.github.io/blockedpage/dbjfvugfdrufvgHUKkgvguifvJYHFHJMTNFHJMugkgiyswiwduguyG(*iuHjbiugf6CR^f5tyf67d7f5d655768iG78gBddhufdgucfdgh.mhtml";
 }
